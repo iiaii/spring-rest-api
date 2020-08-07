bcf0fb0ff6efa42ad3ee6ddf031e1e2c14ddc652
iiaii <jhun9409@naver.com>
2020-08-07 21:19:49 +0900

    findByUsernameFail test

--- src/test/java/me/iiaii/demoinflearnrestapi/accounts/AccountServiceTest.java
+++ src/test/java/me/iiaii/demoinflearnrestapi/accounts/AccountServiceTest.java
@@ -1,22 +1,30 @@
 package me.iiaii.demoinflearnrestapi.accounts;
 
+import org.hamcrest.Matchers;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("test")
 public class AccountServiceTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
     @Autowired
     AccountService accountService;
@@ -46,4 +54,17 @@ public class AccountServiceTest {
         
     }
 
+    @Test
+    public void findByUsernameFail() throws Exception {
+        // given
+        String username = "random@email.com";
+        expectedException.expect(UsernameNotFoundException.class);
+        expectedException.expectMessage(Matchers.containsString(username));
+
+        // when
+        // then
+        UserDetails userDetails = accountService.loadUserByUsername(username);
+
+
+    }
 }
